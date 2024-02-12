358f3143023170c60ca45daa1184cb4a71729d52
MatteoOliva <oliva.matteo98@outlook.com>
2024-02-12 15:37:54 +0100

    feat: footer creato

--- vite-boolando/src/components/AppFooter.vue
+++ vite-boolando/src/components/AppFooter.vue
@@ -3,10 +3,25 @@ export default {};
 </script>
 
 <template>
-    <footer>AppFooter</footer>
+    <footer>
+    <div class="footer-left">
+        <p>boolando s.r.l</p>
+        <p>informazioni legali informativa sulla privacy</p>
+    </div>
+    <div class="footer-right">
+        <div class="contatti"><p>trovaci anche su</p>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-youtube"></i>
+        </div>
+        
+
+    </div>
+</footer>
+    
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 footer {
   width: 100%;
   height: 90px;
@@ -14,5 +29,16 @@ footer {
   margin-top: 100px;
   position: fixed;
   bottom: 0;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  
+  .footer-right {
+    padding-right: 300px;
+  }
+  .footer-left {
+    padding-left: 300px;
+  }
 }
 </style>
